3102dfc9f468d503db616325d21a98cb056258e5
yves_ <yveslal.present@gmail.com>
2024-05-08 09:23:45 +0200

    [Red] shorten url test.

---
+++ test/application/useCases/shortenUrlUseCaseTest.test.ts
@@ -0,0 +1,7 @@
+﻿import {FixtureHelper} from "../../fixtures/helpers/fixtureHelper";
+
+it('should provide a shorter url than the original one', async() => {
+    const fixture: UrlShortenerFixture = await FixtureHelper.load<UrlShortenerFixture>("shortenUrlUseCase/shorter_url_than_the_original_one.json")
+    const link: Link = new Link(fixture.url)
+    expect(link.shorten().length).toBeLessThan(fixture.url.length)
+})
